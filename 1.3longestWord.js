b342445070c7b018d37ccfa8ab7d46b6a9882c31
RipNasty <det625@gmail.com>
2023-11-14 14:34:16 -0500

    1.3

---
+++ 1.3longestWord.js
@@ -0,0 +1,22 @@
+// Given a string representing a sentence, 
+// write a method that returns the longest word. 
+// If there’s a tie, output the longest word that’s found first
+
+const inputString = "David once ate an entire turkey";
+
+const longestWord = (string) => {
+    const splitWords = string.split(' ');
+
+    let currentLongest = ''; 
+
+    for (let i = 0; i < splitWords.length; i++) {
+        if (splitWords[i].length > currentLongest.length) {
+            currentLongest = splitWords[i];
+        }
+    }
+
+    return currentLongest;
+}
+
+console.log(longestWord(inputString));
+
