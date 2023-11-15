17e10d881f6d284729249c6a02c485e93afa09fe
RipNasty <det625@gmail.com>
2023-11-15 00:56:04 -0500

    up to 5.2

---
+++ week5/5.1moreThanOnce.js
@@ -0,0 +1,21 @@
+// Given an array, write a function that returns the list of elements that occur more than once
+
+const array = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6]
+
+const moreThanOnce = (array) => {
+    const frequencyMap = {}
+
+    for (let i = 0; i < array.length; i++) {
+        frequencyMap[array[i]] = (frequencyMap[array[i]] || 0) + 1
+    }
+    const frequencyArray = []
+    for (const key in frequencyMap) {
+        if (frequencyMap[key] > 1) {
+            frequencyArray.push(key)
+        }
+    }
+
+    return frequencyArray
+}
+
+console.log(moreThanOnce(array))
