fa3dc8421cc50b4cb10ab9eda5eb8924e493c996
RipNasty <det625@gmail.com>
2024-03-11 15:30:44 -0400

    1.1redo

--- 1.1redo.js
+++ 1.1redo.js
@@ -3,7 +3,7 @@
 array = [1,2,3,4,5]
 
 const oddNumbers = (array) => {
-    return array.filter(number => number % 2 !== 0)
+    return array.filter((number) => number % 2 !== 0)
 }
 
 console.log (oddNumbers(array))
