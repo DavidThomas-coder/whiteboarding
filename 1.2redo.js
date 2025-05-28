94d88da543b18ef5fdb3d352c738778ad0b95010
RipNasty <det625@gmail.com>
2025-05-28 01:19:45 -0400

    11

--- 1.2redo.js
+++ 1.2redo.js
@@ -2,3 +2,12 @@
 
 array = [1, 2, 3, 4, 5, 6]
 
+function doubleNumbers(array) {
+    let doubled = []
+    for (let i = 0; i < array.length; i++) {
+        doubled.push(array[i] * 2)
+    }
+    return doubled
+}
+
+console.log(doubleNumbers(array))
