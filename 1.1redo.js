7b5641d7ad77244476bd950b3583121f91e081cd
RipNasty <det625@gmail.com>
2025-05-29 01:02:54 -0400

    11

--- 1.1redo.js
+++ 1.1redo.js
@@ -1,4 +1,16 @@
 // Given an array of integers, return a list of all odd numbers found in the list
 
-array = [1,2,3,4,5]
+array = [1, 1, 3, 3, 4, 4, 12, 12, 16, 17, 19]
 
+function oddNumbers(array) {
+    let odds = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 !== 0) {
+            odds.push(array[i])
+        }
+    }
+
+    return odds
+}
+
+console.log(oddNumbers(array))
--- 3.1redo.js
+++ 3.1redo.js
@@ -1,6 +1,8 @@
 // Given an array of integers, create a function that returns true 
 // if all numbers found in the array are even, otherwise return false
 
-arrayEven = [2, 4, 6, 8]
-arrayBoth = [1, 3, 6, 8]
+array = [2, 4, 6, 8]
 
+function isEven(array) {
+    
+}
